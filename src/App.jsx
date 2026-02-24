36af0a276563bfb8d14f79cac8c79d26ef4349f3
Wtc-Klm <incomet2004@gmail.com>
2026-02-24 15:45:00 +0700

    STATE

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,21 @@
 import Contact from './assets/Contact';
+import Counter from './assets/Counter';
 import Hello from './assets/Hello';
-
 
 function App() {
   const helloData = [
-    { name: 'Wuttichai', message: 'Hi there' },
-    { name: 'Top', message: 'Hello..' }
+    { name: 'Anirach', message: 'Hi there' },
+    { name: 'Tom', message: 'Hello..' }
   ];
 
   return (
-    <div >
+    <div className='App'>
+      <Counter />
       {helloData.map((data, index) => (
         <Hello key={index} name={data.name} message={data.message} />
       ))}
 
-      <Contact email="Test@gmail.com" phone ="0817320731" />
+      <Contact email="Anirach@gmail.com" phone="0817320731" />
     </div>
   );
 }
